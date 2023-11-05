8e27cfa19a302eb029cb93fb9e6dc406bf15121c
GALOPIN Antoine <galopin@et.esiea.fr>
2023-11-05 20:59:44 +0100

    Ajout du dossier courrant dans la classe ExplorerController

--- com/esiea/pootd2/controllers/ExplorerController.java
+++ com/esiea/pootd2/controllers/ExplorerController.java
@@ -1,6 +1,8 @@
 package com.esiea.pootd2.controllers;
 
 public class ExplorerController implements IExplorerController {
+    private String currentFolder = "/";
+    
     @Override
     public String executeCommand(String commandStr) {
         return "";
