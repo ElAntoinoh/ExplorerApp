bdc4c655b4fa44aec5ff6a979b4d8bd259905560
GALOPIN Antoine <galopin@et.esiea.fr>
2023-11-24 14:23:43 +0100

    Ajout de l'interface HTTP

--- com/esiea/pootd2/ExplorerApp.java
+++ com/esiea/pootd2/ExplorerApp.java
@@ -1,10 +1,34 @@
 package com.esiea.pootd2;
 
+import java.util.Scanner;
+
 import com.esiea.pootd2.controllers.ExplorerController;
+import com.esiea.pootd2.interfaces.HttpInterface;
 import com.esiea.pootd2.interfaces.TextInterface;
 
 public class ExplorerApp {
+    private final static String TEXT_KEYWORD = "text";
+    private final static String HTTP_KEYWORD = "http";
+
+    private final static String STARTING_STRING = "Mode d'exécution (" + TEXT_KEYWORD + " / " + HTTP_KEYWORD + ") : ";
+
     public static void main(String[] args) {
-        new TextInterface(new ExplorerController()).run();
+        Scanner sc = new Scanner(System.in);
+
+        String input = "";
+
+        do {
+            System.out.print(STARTING_STRING);
+            input = sc.nextLine();
+        } while (!input.equals(TEXT_KEYWORD) && !input.equals(HTTP_KEYWORD));
+
+        ExplorerController explorerController = new ExplorerController();
+
+        switch (input) {
+            case TEXT_KEYWORD -> { new TextInterface(explorerController).run(); }
+            case HTTP_KEYWORD -> { new HttpInterface(explorerController).run(); }
+        }
+
+        sc.close();
     }
 }
