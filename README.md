c5541bc953551fde51c7818773be2edcd2a7f645
GALOPIN Antoine <galopin@et.esiea.fr>
2023-11-26 23:35:55 +0100

    Modification du README.md

--- README.md
+++ README.md
@@ -1,8 +1,66 @@
 # ExplorerApp
 
-Compilation : {
-    Linux : javac @compile.list
-    Windows : javac "@compile.list"
-}
+## Utilisation du projet
+### Compilation
+Linux : javac @compile.list
+Windows : javac "@compile.list"
 
-Execution : java com.esiea.pootd2.ExplorerApp
+### Execution
+java com.esiea.pootd2.ExplorerApp
+
+## Documentation
+
+### Liste des commandes
+
+#### cd (ChangeDirectory)
+__Particularités__:
+"cd" équivaut à "cd /"
+
+__Capacités__:
+Possibilité de suivre des chemins relatifs ainsi que des chemins absolus
+Possibilité de suivre un chemin nécessitant de traverser plusieurs dossiers
+Possibilité d'utiliser les mots clés ".." et "."
+Aucun déplacement résiduel en cas d'échec de la commande
+
+__Erreurs possibles à l'utilisation__:
+Trop d'arguments passés en paramètres : "cd: trop d'arguments"
+Tentative de déplacement dans un inode innexistant : "cd: [inode]: Aucun fichier ou dossier de ce type"
+Tentative de déplacement dans un fichier : "cd: [inode]: N'est pas un dossier"
+
+#### ls (List)
+__Particularités__:
+Une ligne par fichier fils
+
+Structure de l'affichage d'un fichier : type_fichier, nom_fichier, poids_fichier
+
+type_fichier :
+    - d : répertoire
+    - f : autre
+
+__Pistes d'amélioration__:
+Possibilité d'exécuter la commande sur des dossiers passés en paramètre
+
+#### mkdir (MakeDirectory)
+__Capacités__:
+Possibilité de créer plusieurs dossiers à la fois
+
+__Erreurs possibles à l'utilisation__:
+Pas assez d'arguments passés en paramètres : "mkdir: opérande manquant"
+Tentative de création d'un répertoire dont le nom est déjà pris : "mkdir: impossible de créer le répertoire [répertoire]: Le fichier existe"
+
+__Pistes d'amélioration__:
+Possibilité de suivre des chemins relatifs ainsi que des chemins absolus
+Possibilité de suivre un chemin nécessitant de traverser plusieurs dossiers
+Possibilité d'utiliser les mots clés ".." et "."
+
+#### touch (Touch)
+__Capacités__:
+Possibilité de touch plusieurs fichiers à la fois
+
+__Erreurs possibles à l'utilisation__:
+Pas assez d'arguments passés en paramètres : "touch: opérande de fichier manquant"
+
+__Pistes d'amélioration__:
+Possibilité de suivre des chemins relatifs ainsi que des chemins absolus
+Possibilité de suivre un chemin nécessitant de traverser plusieurs dossiers
+Possibilité d'utiliser les mots clés ".." et "."
--- com/esiea/pootd2/controllers/ExplorerController.java
+++ com/esiea/pootd2/controllers/ExplorerController.java
@@ -127,8 +127,82 @@ public class ExplorerController implements IExplorerController {
     private String doCommand(MakeDirectoryCommand command) {
         String s = "";
 
+        FolderInode currentFolderCopy;
+
         String[] args = command.getArgs();
 
+        // Vérification du nombre d'arguments
+        if (args.length >= 1) {
+            // Parcours des arguments
+            for (String arg : args) {
+                // Chemin virtuel pour s'assurer de la possiblité du déplacement
+                currentFolderCopy = currentFolder;
+
+                // Cas d'un chemin absolu
+                if (arg.startsWith("/")) {
+                    arg = arg.substring(1, arg.length());
+                    currentFolderCopy = goToRoot(currentFolderCopy);
+                }
+
+                // Récupération des étapes du chemin du dossier à créer
+                String[] steps = arg.split("/");
+
+                // Parcours des étapes du chemin du dossier à créer
+                for (int i = 0; i < steps.length; i++) {
+                    if (i != steps.length - 1) {
+                        Inode child = currentFolderCopy.findChild(steps[0]);
+
+                        // Si un enfant est trouvé
+                        if (child != null) {
+                            // Si l'enfant trouvé est un dossier
+                            if (child instanceof FolderInode) {
+                                currentFolderCopy = (FolderInode) child;
+                            }
+
+                            // Si l'enfant trouvé est un fichier
+                            else {
+                                s += "mkdir: impossible de créer le répertoire \"" + arg + "\": N'est pas un dossier";
+                            }
+                        }
+
+                        // Si aucun enfant n'est trouvé
+                        else {
+                            s += "mkdir: impossible de créer le répertoire \"" + arg + "\": Aucun fichier ou dossier de ce type";
+                        }
+                    }
+
+                    else {
+                        if (currentFolderCopy.findChild(arg) == null) {
+                            currentFolderCopy.addInode(new FolderInode(arg));
+                        } else {
+                            if (!s.equals("")) s += "\n";
+
+                            s += "mkdir: impossible de créer le répertoire \"" + arg + "\": Le fichier existe";
+                        }
+                    }
+                }
+            }
+        } else {
+            s = "mkdir: opérande manquant";
+        }
+
+        /*
+        if (args.length >= 1) {
+            for (String arg : args) {
+                if (this.currentFolder.findChild(arg) == null) {
+                    this.currentFolder.addInode(new FolderInode(arg));
+                } else {
+                    if (!s.equals("")) s += "\n";
+
+                    s += "mkdir: impossible de créer le répertoire \"" + arg + "\": Le fichier existe";
+                }
+            }
+        } else {
+            s = "mkdir: opérande manquant";
+        }
+        */
+
+        /*
         if (args.length >= 1) {
             for (int i = 0; i < args.length; i++) {
                 if (this.currentFolder.findChild(args[i]) == null) {
@@ -142,6 +216,7 @@ public class ExplorerController implements IExplorerController {
         } else {
             s = "mkdir: opérande manquant";
         }
+        */
 
         return s;
     }
